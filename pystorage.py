76160df259720ef401e58c30d2a98dda0a60d9d8
MBSA13102002 <73106667+MBSA13102002@users.noreply.github.com>
2021-04-15 00:19:51 +0530

    commit

--- pystorage.py
+++ pystorage.py
@@ -1,5 +1,4 @@
-from tkinter import *
-root=Tk()
+
 import pyrebase
 config ={
   "apiKey": "AIzaSyCCF5YTqq35piJ8B6Od7ymPthLqmqLwsxE",
@@ -10,7 +9,7 @@ config ={
     "serviceAccount":"pybase-44f1b-firebase-adminsdk-cvqbt-d2e92b4ae1.json"
 }
 
-# firebase_storage = pyrebase.initialize_app(config) 
-# storage=firebase_storage.storage()
-# storage.child("D:/wifi.txt").put("D:/wifi.txt")
-root.mainloop()
+firebase_storage = pyrebase.initialize_app(config) 
+storage=firebase_storage.storage()
+storage.child("D:/wifi.txt").put("D:/wifi.txt")
+
